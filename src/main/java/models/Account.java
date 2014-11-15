818e28d660b4311112dc575ac7944b08d1a4dfcc
Red Moses <musa@redmoses.me>
2014-11-15 20:19:41 +0600

    Started working with Account Model

---
+++ src/main/java/models/Account.java
@@ -0,0 +1,11 @@
+package models;
+
+import policy.AccountPolicy;
+
+/**
+ * Created by red on 11/15/14.
+ */
+public class Account {
+    public AccountPolicy.AccountRole AccountType;
+
+}
---
+++ src/main/java/policy/AccountPolicy.java
@@ -0,0 +1,11 @@
+package policy;
+
+/**
+ * Created by red on 11/15/14.
+ */
+public class AccountPolicy {
+    public enum AccountRole {
+        TESTER,
+        PUPIL
+    }
+}
