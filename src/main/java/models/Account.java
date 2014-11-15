a870d4e8c909d542f52235c405091958f3d3e053
Red Moses <musa@redmoses.me>
2014-11-15 20:29:17 +0600

    Partially created account model.

--- src/main/java/models/Account.java
+++ src/main/java/models/Account.java
@@ -2,10 +2,52 @@ package models;
 
 import policy.AccountPolicy;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Created by red on 11/15/14.
+ * Created by Red on 11/15/14.
+ * Description: User account Model
+ *  *
  */
+
+@Entity
 public class Account {
-    public AccountPolicy.AccountRole AccountType;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private AccountPolicy.AccountRole AccountType;
+
+    @JsonIgnore
+    public String password;
+    public String username;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Account(String name, String password) {
+        this.username = name;
+        this.password = password;
+    }
+
+    Account() { // jpa only
+    }
 
 }
